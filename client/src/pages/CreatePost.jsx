c9cb7f4742fcbce1a6591f710e1a8ca31ade4690
Jamie Elliott <jamie.anne.elliott@gmail.com>
2023-01-25 16:00:50 -0600

    update config

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-dom';
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
